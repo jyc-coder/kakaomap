046920d712e606364202c4e3edf59f203adb2e02
jyc-coder <jyc4648@gmail.com>
2022-09-19 17:27:49 +0900

    #2 - kakaoMap api를 사용한 지도 구현
    여러가지 방법을 사용하여 kakaoMap을 구현한다.
    
    * `public/index.html`에 script를 작성(api키와 세부내용 모두 작성) 한 다음 불러오기 #1
    
    * `public/index.html`에 script를 작성(api키)하고 세부내용은 `App`에 작성한 다음 api만 불러오기 #2
    
    * #2방법에서 `getElementById`를 대신하는 `useRef`를 사용하여 Element를 선택하는 방법 #3
    
    * `useEffect`에 api키와 세부내용을 모두 작성하는 방법 #4

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import "./App.css";
+
+declare global {
+  interface Window {
+    kakao: any;
+    loadMap: () => void;
+  }
+}
 
 function App() {
+  const mapRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    //#1 index.html에 script를 작성(api키와 세부내용 모두 작성)하고 불러오기
+    //window.loadMap()
+
+    //#2 index.html에 script를 작성(api키만 작성하고 세부내용을 작성함)
+    /*  const container = document.getElementById("map");
+    var options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    var map = new window.kakao.maps.Map(container, options); */
+
+    //#3 Id를 사용하는 대신에 useRef를 사용하여 Element를 선택함
+    /*   if (mapRef.current) {
+      var options = {
+        center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+      };
+
+      var map = new window.kakao.maps.Map(mapRef.current, options);
+
+      console.log(mapRef.current);
+    } */
+
+    //#4 useEffect에 api키와 세부내용을 모두 작성(권장)
+    const script = document.createElement("script");
+
+    script.src =
+      "//dapi.kakao.com/v2/maps/sdk.js?appkey=beef395ac58f9ef890b8b34d251e5fa0&autoload=false";
+
+    document.head.appendChild(script);
+
+    script.onload = () => {
+      console.log("hello");
+      window.kakao.maps.load(() => {
+        if (mapRef.current) {
+          var options = {
+            center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+            level: 3,
+          };
+
+          var map = new window.kakao.maps.Map(mapRef.current, options);
+
+          console.log(mapRef.current);
+        }
+      });
+    };
+
+    return () => script.remove();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      ref={mapRef}
+      style={{
+        width: 300,
+        height: 300,
+      }}
+    >
+      뭐요
     </div>
   );
 }
